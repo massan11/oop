ca2e40209881ad1a7aa2959131035c9384e1933d
massan11 <mhassan.zoho@gmail.com>
2025-01-26 16:53:14 +0330

    the simplest class added to simple-class.py

---
+++ exercise/simple-class.py
@@ -0,0 +1,18 @@
+class Employee:
+    pass
+
+emp1 = Employee()
+emp2 = Employee()
+
+print (emp1)
+print (emp2)
+
+emp1.name = "ali"
+emp1.age = 29
+
+emp2.name = "mamad"
+emp2.age = 28
+
+print (emp1.name, emp1.age)
+
+print (emp2.name, emp2.age)
