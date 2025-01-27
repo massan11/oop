8ea55d04e7eb390ded5d2c6224e24dd3e8fccec1
massan11 <mhassan.zoho@gmail.com>
2025-01-27 21:19:40 +0330

    Employee.py updated

--- exercise/Employee.py
+++ exercise/Employee.py
@@ -5,10 +5,15 @@ class Employee:
         self.name = name #object variable
         self.age = age
 
+    def work(self, project_name):
+        return f"I am working on {project_name}"
+    def introdoce(self):
+        return f"my name is {self.name}"
+
 emp1 = Employee("hassan", 32)
 emp2 = Employee("ali", 43)
 
 print(emp1.name, emp1.age, emp1.salary)
-print(emp2.name, emp2.age, emp2.salary)
+print(emp1.introdoce())
 
 
